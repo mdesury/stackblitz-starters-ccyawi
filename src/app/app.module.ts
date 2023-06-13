7fd85ad0d6f57e66f0ef647c9095e09676d0c843
mdesury <127192048+mdesury@users.noreply.github.com>
2023-06-13 20:51:18 +0200

    1 file added

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Service } from './Service/Service';
+import { AppComponent } from './app.component';
+import { MyComponent } from './my.component';
+import { RootComponent } from './Components/root/root.component';
+
+@NgModule({
+  declarations: [AppComponent, MyComponent, RootComponent],
+
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
